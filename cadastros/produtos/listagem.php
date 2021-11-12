0df923114d458d14edb6ab2d101890f3b06edd94
Rafael-Boeing24 <54421772+Rafael-Boeing24@users.noreply.github.com>
2021-11-12 16:41:08 -0300

    Ajustado para que na tela inicial, seja carregado a consulta de Produtos

--- cadastros/produtos/listagem.php
+++ cadastros/produtos/listagem.php
@@ -1,8 +1,51 @@
 <?php
 
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+try {
+    $stmt = $conn->prepare('select id,' . ENTER .
+                           '       marca,' . ENTER .
+                           '       nome,' . ENTER .
+                           '       quantidade,' . ENTER .
+                           '       valorunitario,' . ENTER .
+                           '       round((valorunitario * quantidade), 2) as valortotal' . ENTER .
+                           'from produtos');
+    $stmt->execute();
 
+    //while($row = $stmt->fetch()) {
+    //while($row = $stmt->fetch(PDO::FETCH_OBJ)) {
+    //print_r($row);
+    //}
+
+    $result = $stmt->fetchAll();
+    ?>
+    <table border="1" class="table table-striped">
+        <tr>
+            <td>ID</td>
+            <td>Marca</td>
+            <td>Nome</td>
+            <td>Quantidade</td>
+            <td>Valor Unitário</td>
+            <td>Valor Total</td>
+        </tr>
+        <?php
+        if (count($result)) {
+            foreach ($result as $row) {
+                ?>
+                <tr>
+                    <td><?= $row['id'] ?></td>
+                    <td><?= $row['marca'] ?></td>
+                    <td><?= $row['nome'] ?></td>
+                    <td><?= $row['quantidade'] ?></td>
+                    <td><?= $row['valorunitario'] ?></td>
+                    <td><?= $row['valortotal'] ?></td>
+                </tr>
+                <?php
+            }
+        } else {
+            echo "Nenhum resultado retornado.";
+        }
+        ?>
+    </table>
+    <?php
+} catch (PDOException $e) {
+    echo 'ERROR: ' . $e->getMessage();
+}
